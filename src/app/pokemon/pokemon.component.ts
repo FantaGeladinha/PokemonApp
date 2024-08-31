c6df97809d536cdb39bbcf403cbb0647e4796acf
Cursos FIC <danilorodriguesmariano@gmail.com>
2024-08-31 16:15:27 -0300

    aa

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -6,47 +6,41 @@ import { PokemonService } from './pokemon.service';
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
 })
-export class PokemonComponent  implements OnInit {
+export class PokemonComponent implements OnInit {
 
-  public pokemons: any = []
+  public pokemons: any[] = [];
   public isModalOpen = false;
   public selectedPokemon: any;
-  public pesquisarTermo: string ='';
+  public pesquisarTermo: string = '';
   public filtroPokemon: any[] = [];
+
   constructor(private pokemonService: PokemonService) { }
-
-
 
   ngOnInit() {
     this.pokemonService.getPokemon().subscribe((data) => {
-      this.pokemons = data.results
-      this.filtroPokemon = this.pokemons
-    })
+      this.pokemons = data.results;
+      this.filtroPokemon = this.pokemons;
+    });
   }
 
-  fecharModal(){
-    this.isModalOpen = false
+  fecharModal() {
+    this.isModalOpen = false;
   }
 
-  openModal(pokemon: any){
+  openModal(pokemon: any) {
     this.pokemonService.getPokemonDetalhes(pokemon.name).subscribe((detalhes) => {
-      this.selectedPokemon = detalhes
+      this.selectedPokemon = detalhes;
       this.isModalOpen = true;
-    })
-
+    });
   }
 
-  filtrarPokemon(){
-    if (!this.pesquisarTermo.trim()){
-        this.filtroPokemon = this.pokemons;
-
-      }
-
-      else {
-        this.filtroPokemon = this.pokemons.filter((p: any) =>
-        p.name.tolowerCase().includes.this.pesquisarTermo.toLocaleLowerCase()
-        );
-      }
+  filtrarPokemon() {
+    if (!this.pesquisarTermo.trim()) {
+      this.filtroPokemon = this.pokemons;
+    } else {
+      this.filtroPokemon = this.pokemons.filter((p: any) =>
+        p.name.toLowerCase().includes(this.pesquisarTermo.toLowerCase())
+      );
+    }
   }
-
 }
